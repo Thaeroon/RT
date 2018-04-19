a1a3edb63321d8da922f89392478e65b893c9418
Nicolas MULLER <nmuller@e2r8p14.42.fr>
2018-04-19 19:15:31 +0200

    norme utils

--- srcs/utils/read_raw_input.c
+++ srcs/utils/read_raw_input.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_raw_input.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmuller <nmuller@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/19 19:14:15 by nmuller           #+#    #+#             */
+/*   Updated: 2018/04/19 19:15:16 by nmuller          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "utils.h"
 
@@ -11,18 +23,19 @@ static char			*get_raw_input(void)
 	}
 	else
 	{
-		twl_printf("If you hav'nt pipe any content in the stdin, please hit ctrl + D\n");
+		twl_printf("If you haven't pipe any content in the stdin,\
+													please hit ctrl + D\n");
 		raw_input = twl_fd_to_str(STDIN_FILENO);
 	}
 	return (raw_input);
 }
 
-char 				*read_raw_input()
+char				*read_raw_input(void)
 {
 	char			*raw_input;
 
 	raw_input = get_raw_input();
-	if (! raw_input || twl_strlen(raw_input) == 0)
+	if (!raw_input || twl_strlen(raw_input) == 0)
 		twl_xprintf("Empty input");
 	return (raw_input);
 }
