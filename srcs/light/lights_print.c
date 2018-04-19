87ae1a39d0b3d68dd4d0ef1ab1741adc8d2b2955
Nicolas MULLER <nmuller@e2r8p14.42.fr>
2018-04-19 20:21:48 +0200

    norme light

--- srcs/light/lights_print.c
+++ srcs/light/lights_print.c
@@ -1,11 +1,21 @@
-#include "light.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lights_print.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmuller <nmuller@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/19 20:21:13 by nmuller           #+#    #+#             */
+/*   Updated: 2018/04/19 20:21:18 by nmuller          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "light.h"
 
 static void			print_fn(void *elem)
 {
 	light_print((t_light *)elem);
 }
-
 
 void				lights_print(t_lst *lights)
 {
