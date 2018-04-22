9dbf1298732eb9f3096457041213920893dec367
pierrebeller <pierre.beller@gmail.com>
2018-04-22 10:53:34 +0200

    norme

--- srcs/camera/build_camera_from_dict.c
+++ srcs/camera/build_camera_from_dict.c
@@ -20,15 +20,15 @@ static void			suite(t_camera *camera, t_dict *dict)
 	char		*ret_val;
 
 	ret_val = dict_get_with_default(dict, "sepia_filter", "0");
-	camera->sepia_filter = atoi(ret_val);
+	camera->sepia_filter = twl_atoi(ret_val);
 	ret_val = dict_get_with_default(dict, "save_as_ppm", "0");
-	camera->save_as_ppm = atoi(ret_val);
+	camera->save_as_ppm = twl_atoi(ret_val);
 	ret_val = dict_get_with_default(dict, "sky_background", "1");
-	camera->sky_background = atof(ret_val);
+	camera->sky_background = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "min_illumination", "0");
-	camera->min_illumination = atof(ret_val);
+	camera->min_illumination = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "save", "0");
-	camera->min_illumination = atoi(ret_val);
+	camera->min_illumination = twl_atoi(ret_val);
 }
 
 void				build_camera_from_dict(t_camera *camera, t_dict *dict)
@@ -36,24 +36,24 @@ void				build_camera_from_dict(t_camera *camera, t_dict *dict)
 	char			*ret_val;
 
 	ret_val = dict_get_with_default(dict, "pos.x", "0");
-	camera->pos.x = atof(ret_val);
+	camera->pos.x = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "pos.y", "0");
-	camera->pos.y = atof(ret_val);
+	camera->pos.y = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "pos.z", "0");
-	camera->pos.z = atof(ret_val);
+	camera->pos.z = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "dir_x", "0");
-	camera->dir_x = atof(ret_val);
+	camera->dir_x = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "dir_y", "0");
-	camera->dir_y = atof(ret_val);
+	camera->dir_y = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "dir_z", "0");
-	camera->dir_z = atof(ret_val);
+	camera->dir_z = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "fov", "90");
-	camera->fov = atof(ret_val);
+	camera->fov = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "up_x", "0");
-	camera->up_x = atof(ret_val);
+	camera->up_x = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "up_y", "1");
-	camera->up_y = atof(ret_val);
+	camera->up_y = twl_atof(ret_val);
 	ret_val = dict_get_with_default(dict, "up_z", "0");
-	camera->up_z = atof(ret_val);
+	camera->up_z = twl_atof(ret_val);
 	suite(camera, dict);
 }
